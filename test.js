0891e1a1bebc9be60faddd3adeb9b730a475d96f
Alvin Dizon <76100624+alvindizon21@users.noreply.github.com>
2020-12-21 05:02:51 +0800

    Add files via upload

---
+++ test.js
@@ -0,0 +1,12 @@
+// the first alert
+alert("Hello, World!");
+
+// the second alert
+alert("Hello, World!");
+
+//move the image from top to buttom
+//make the image invisible
+//make the image visible
+
+
+
